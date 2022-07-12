ea10179c9eb1d65677465d50a834284cba31ce04
portaldif <107978888+portaldif@users.noreply.github.com>
2022-07-12 18:49:32 -0500

    Add files via upload

--- js/slide.js
+++ js/slide.js
@@ -15,5 +15,25 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Cambia cada 4 segundos
+}
+
+let slideIndexAds = 0;
+showSlidesAds();
+
+function showSlidesAds() {
+  let i;
+  let slidesads = document.getElementsByClassName("mySlides-ads");
+  let dotsx = document.getElementsByClassName("dots");
+  for (i = 0; i < slidesads.length; i++) {
+    slidesads[i].style.display = "none";  
+  }
+  slideIndexAds++;
+  if (slideIndexAds > slidesads.length) {slideIndexAds = 1}    
+  for (i = 0; i < dotsx.length; i++) {
+    dotsx[i].className = dotsx[i].className.replace(" active-ads", "");
+  }
+  slidesads[slideIndexAds-1].style.display = "block";  
+  dotsx[slideIndexAds-1].className += " active-ads";
+  setTimeout(showSlidesAds, 4000); // Cambia cada 4 segundos
 }
